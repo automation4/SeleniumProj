84ae995d3d3e3bb184a97cfd9929aad9cac23ddb
himanshuP <himanshuP@gmail.com>
2022-04-09 17:58:40 +0530

    masterchanges3

--- src/test/java/com/tests/RahulshettyAcademyTests.java
+++ src/test/java/com/tests/RahulshettyAcademyTests.java
@@ -38,7 +38,7 @@ public class RahulshettyAcademyTests extends TestBase{
         driver.get(Utils.getInstance().getdata("testurl1"));
         rsh.getRSSection1().selectcountry("India");
         Thread.sleep(5000);
-        System.out.println("Country selected3");
+        System.out.println("Country selected5");
     }
 
     @Test(groups = "smoke")
